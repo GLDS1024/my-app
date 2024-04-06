48dd6c387d72b9a54bc86ed680bc48728957f000
glds1024 <ggllddss2022@gmail.com>
2024-04-06 22:27:41 +0800

    2024-04-06

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,14 @@ const app = new Hono<{ Bindings: Bindings }>()
 app.use(prettyJSON())
 app.notFound((c) => c.json({ message: 'Not Found', ok: false }, 404))
 app.get('/', (c) => c.text('GLDS1024 WEB API'))
+app.get("/query/users", async (c) => {
+  try {
+    let { results } = await c.env.DB.prepare("SELECT * FROM users").all()
+    return c.json(results)
+  } catch (e) {
+    return c.json({err: e}, 500)
+  }
+})
 app.get("/query/users/:id", async (c) => {
   const userId= c.req.param("id")
   try {
